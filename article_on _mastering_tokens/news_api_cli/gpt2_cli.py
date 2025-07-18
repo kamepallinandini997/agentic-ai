71c5fe98887f6d77882fe129cb6c3c815601645b
Nandini Kamepalli <nandini.kamepalli@thezennialpro.com>
2025-07-18 18:26:14 +0530

    Added File structure for practical example mentioned in artcle

---
+++ article_on _mastering_tokens/news_api_cli/gpt2_cli.py
@@ -0,0 +1,13 @@
+
+"""
+Work in progress 
+
+"""
+
+
+def main():
+    print(" GPT-2 CLI coming soon...")
+
+if __name__ == "__main__":
+    main()
+
---
+++ article_on _mastering_tokens/news_api_cli/gpt4_cli.py
@@ -0,0 +1,10 @@
+
+"""
+Work in progress 
+
+"""
+def main():
+    print("GPT-4 CLI coming soon...")
+
+if __name__ == "__main__":
+    main()
